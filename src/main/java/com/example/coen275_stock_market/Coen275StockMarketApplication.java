39872a1069e89e6664154cd1ec5f7f78cc764ba1
ShuangchangLi <89339613+ShuangchangLi@users.noreply.github.com>
2022-05-18 20:01:21 -0700

    First commit

---
+++ src/main/java/com/example/coen275_stock_market/Coen275StockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.coen275_stock_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coen275StockMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coen275StockMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coen275_stock_market/Coen275StockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coen275_stock_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coen275StockMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
